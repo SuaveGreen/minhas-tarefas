f927ff92f4cc90bf4cdb0d5c01edf6101815d51f
Pedro Caetano <pedro.kaetano2003@gmail.com>
2024-02-12 18:51:17 -0300

    Adicionando a funcionalidade de filtro pro TAGs

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
-
-import * as S from './styles'
 import { RootReducer } from '../../store'
 import { alterarTermo } from '../../store/reducers/filtro'
+
+import * as S from './styles'
+import * as enums from '../../Utils/enums/Tarefa'
 
 const BarraLateral = () => {
   const dispatch = useDispatch()
@@ -19,12 +20,36 @@ const BarraLateral = () => {
           onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
         />
         <S.Filtros>
-          <FiltroCard legenda="pendentes" contador={1} />
-          <FiltroCard legenda="concluidas" contador={2} />
-          <FiltroCard legenda="urgentes" contador={3} />
-          <FiltroCard legenda="importantes" contador={4} />
-          <FiltroCard legenda="normal" contador={5} />
-          <FiltroCard legenda="todas" contador={6} ativo />
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="Status"
+            legenda="Pendentes"
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="Status"
+            legenda="Concluidas"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="Prioridade"
+            legenda="Urgentes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="Prioridade"
+            legenda="Importantes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="Prioridade"
+            legenda="Normal"
+          />
+          <FiltroCard
+            // valor={enums.Prioridade.PENDENTE}
+            criterio="Prioridade"
+            legenda="Todas"
+          />
         </S.Filtros>
       </div>
     </S.Aside>
